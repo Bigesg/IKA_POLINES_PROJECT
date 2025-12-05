6756197c586ff36d2776322c3a92ef143bb4d681
madaniaaldanita-blip <madaniaaldanita@gmail.com>
2025-12-05 11:16:16 +0700

    Update test_navigation.dart

--- lib/test_navigation.dart
+++ lib/test_navigation.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'navigator.dart';
 
+void main() {
+  runApp(const TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'IKA POLINES Navigation Test',
+      home: MainAppNavigator(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
